03ea9b6f40d590f2150cb55cc3d5f83b76f0eb73
Kanishk Garg <61554323+KanishkGar@users.noreply.github.com>
2020-11-24 23:26:38 -0600

    Finished all the basic functionalities of the app

--- app/src/main/java/kanishk/example/basiccalculator/MainActivity.java
+++ app/src/main/java/kanishk/example/basiccalculator/MainActivity.java
@@ -7,41 +7,73 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 
 public class MainActivity extends AppCompatActivity {
 
     String eval = "";
-    TextView text = findViewById(R.id.text);
+    TextView text;
     //All of the buttons
-    Button clear = findViewById(R.id.clear);
-    Button delete = findViewById(R.id.delete);
-    Button enter = findViewById(R.id.enter);
+    Button clear;
+    Button delete ;
+    Button enter;
 
-    Button zero = findViewById(R.id.clear);
-    Button paren = findViewById(R.id.paren);
-    Button paren2 = findViewById(R.id.paren2);
-    Button plus = findViewById(R.id.plus);
+    Button zero ;
+    Button paren;
+    Button paren2 ;
+    Button plus ;
 
-    Button seven = findViewById(R.id.seven);
-    Button eight = findViewById(R.id.eight);
-    Button nine = findViewById(R.id.nine);
-    Button minus = findViewById(R.id.minus);
+    Button seven;
+    Button eight;
+    Button nine ;
+    Button minus ;
 
-    Button four = findViewById(R.id.four);
-    Button five = findViewById(R.id.five);
-    Button six = findViewById(R.id.six);
-    Button multiply = findViewById(R.id.multiply);
+    Button four ;
+    Button five;
+    Button six ;
+    Button multiply ;
 
-    Button one = findViewById(R.id.one);
-    Button two = findViewById(R.id.two);
-    Button three = findViewById(R.id.three);
-    Button divide = findViewById(R.id.divide);
+    Button one;
+    Button two ;
+    Button three;
+    Button divide;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text);
+        //All of the buttons
+
+
+         clear = findViewById(R.id.clear);
+         delete = findViewById(R.id.delete);
+         enter = findViewById(R.id.enter);
+
+         zero = findViewById(R.id.zero);
+         paren = findViewById(R.id.paren);
+         paren2 = findViewById(R.id.paren2);
+         plus = findViewById(R.id.plus);
+
+         seven = findViewById(R.id.seven);
+         eight = findViewById(R.id.eight);
+         nine = findViewById(R.id.nine);
+         minus = findViewById(R.id.minus);
+
+         four = findViewById(R.id.four);
+         five = findViewById(R.id.five);
+         six = findViewById(R.id.six);
+         multiply = findViewById(R.id.multiply);
+
+         one = findViewById(R.id.one);
+         two = findViewById(R.id.two);
+         three = findViewById(R.id.three);
+         divide = findViewById(R.id.divide);
 
         //all of ze buttons
         clear.setOnClickListener(new View.OnClickListener() {
@@ -55,15 +87,151 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(eval.length()>0)
-                eval = eval.substring(eval.length()-2);
+                    eval = eval.substring(0,eval.length()-1);
                 text.setText(eval);
             }
         });
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(eval.length() == 0)
+                    return;
+                ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+                Object result = null;
+                Integer in = null;
+                String db = null;
+                try {
+                    result = engine.eval(eval);
+                } catch (ScriptException e) {
+                    e.printStackTrace();
+                }
+//                try {
+//                    in = (Integer)result;
+//                } catch (ClassCastException e) {
+//                    db = (String)result;
+//                }
+//                if(in==null)
+//                    text.setText(String.valueOf(db));
+//                else
+//                    text.setText(in);
+                text.setText(result.toString());
+                eval = "";
+            }
+        });
 
-                //text.setText(eval);
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "0";
+                text.setText(eval);
+            }
+        });
+        paren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "(";
+                text.setText(eval);
+            }
+        });
+        paren2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += ")";
+                text.setText(eval);
+            }
+        });
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "+";
+                text.setText(eval);
+            }
+        });
+
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "7";
+                text.setText(eval);
+            }
+        });
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "8";
+                text.setText(eval);
+            }
+        });
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "9";
+                text.setText(eval);
+            }
+        });
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "-";
+                text.setText(eval);
+            }
+        });
+
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "4";
+                text.setText(eval);
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "5";
+                text.setText(eval);
+            }
+        });
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "6";
+                text.setText(eval);
+            }
+        });
+        multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "*";
+                text.setText(eval);
+            }
+        });
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "1";
+                text.setText(eval);
+            }
+        });
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "2";
+                text.setText(eval);
+            }
+        });
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "3";
+                text.setText(eval);
+            }
+        });
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eval += "/";
+                text.setText(eval);
             }
         });
 
